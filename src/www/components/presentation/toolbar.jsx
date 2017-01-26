fef5b9e94e20fd7965cf26993ae7e1bf9cc4df20
Rua Haszard <r.haszard@kuracloud.com>
2017-01-26 16:51:33 +1300

    make Toolbar a class, ready for some local state

--- src/www/components/presentation/toolbar.jsx
+++ src/www/components/presentation/toolbar.jsx
@@ -7,48 +7,51 @@ import React, { PropTypes } from 'react';
 
 import colours from '../../styles/colours';
 
+class Toolbar extends React.Component {
+   render() {
+      let { playState, beatNumber, onPlayClick, onToggleEditMode, editMode, exportData } = this.props;
 
-const Toolbar = ({ playState, beatNumber, onPlayClick, onToggleEditMode, editMode, exportData }) => {
-   let playButtonIconClass = '';
-   if (playState == "playing")
-      playButtonIconClass = 'icon-stop';
-   else if (playState == "stopped")
-       playButtonIconClass = 'icon-play';
-   let editButtonStyle = {};
-   if (editMode)
-      editButtonStyle = { color: colours.enabledButtonForeground };
+      let playButtonIconClass = '';
+      if (playState == "playing")
+         playButtonIconClass = 'icon-stop';
+      else if (playState == "stopped")
+          playButtonIconClass = 'icon-play';
+      let editButtonStyle = {};
+      if (editMode)
+         editButtonStyle = { color: colours.enabledButtonForeground };
 
-   let onExportSessionClicked = () => {
-      let projectName = 'kytaime';
-      let sessionFileName = projectName + '-' + moment().format('YYYYMMDD-hhmmss') + '.kytaime';
-      let dataURI = 'data:Application/octet-stream,' + encodeURIComponent(exportData);
-      var blob = new Blob([exportData], {type: "text/hjson;charset=utf-8"});
-      FileSaver.saveAs(blob, sessionFileName);
-   }
-  
-   return ( 
-      <section className="toolbar noSelect">
-         <div className="row expanded">
-            <div className="shrink columns">
-               <div className="icon-pencil" style={editButtonStyle} 
-                  onClick={onToggleEditMode}></div>
-            </div>
-            <div className="shrink columns">
-               <div className="icon-menu"
-                  onClick={onExportSessionClicked}></div>
-            </div>
-            <div className="small columns text-center">
-               <div className="row align-center">
-                  <div className="">{beatNumber}</div>
-                  {/* <div className="">/32</div> */}
+      let onExportSessionClicked = () => {
+         let projectName = 'kytaime';
+         let sessionFileName = projectName + '-' + moment().format('YYYYMMDD-hhmmss') + '.kytaime';
+         let dataURI = 'data:Application/octet-stream,' + encodeURIComponent(exportData);
+         var blob = new Blob([exportData], {type: "text/hjson;charset=utf-8"});
+         FileSaver.saveAs(blob, sessionFileName);
+      }
+     
+      return ( 
+         <section className="toolbar noSelect">
+            <div className="row expanded">
+               <div className="shrink columns">
+                  <div className="icon-pencil" style={editButtonStyle} 
+                     onClick={onToggleEditMode}></div>
+               </div>
+               <div className="shrink columns">
+                  <div className="icon-menu"
+                     onClick={onExportSessionClicked}></div>
+               </div>
+               <div className="small columns text-center">
+                  <div className="row align-center">
+                     <div className="">{beatNumber}</div>
+                     {/* <div className="">/32</div> */}
+                  </div>
+               </div>
+               <div className="shrink columns text-right" onClick={onPlayClick}>
+                  <div className={playButtonIconClass} ></div>
                </div>
             </div>
-            <div className="shrink columns text-right" onClick={onPlayClick}>
-               <div className={playButtonIconClass} ></div>
-            </div>
-         </div>
-      </section>
-   );
+         </section>
+      );      
+   }
 }
 
 Toolbar.propTypes = {
