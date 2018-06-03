a7b7277f782a960f64344f21bd21959823d85591
haszari <haszari@cartoonbeats.com>
2018-06-03 17:16:49 +1200

    fix whitespace (2 spaces)

--- src/www/apps/throwdown/throwdown.jsx
+++ src/www/apps/throwdown/throwdown.jsx
@@ -9,74 +9,74 @@ import _ from 'lodash';
 const audioPath = '/audio/stems/';
 
 const throwdowns = {
-   mivova: {
-      name: 'mivova',
-      tempo: 116,
-      key: 'Am',
-      patterns: {
-         beat: {
-            audio: audioPath + '20180425--mivova--padscape--beat.mp3',
-            tempo: 122,
-            duration: 8,
-            part: 'drums',
-            startBeats: [0, 3],
-            endBeats: [0.5],
-         }
-      }   
-   }
+  mivova: {
+    name: 'mivova',
+    tempo: 116,
+    key: 'Am',
+    patterns: {
+      beat: {
+        audio: audioPath + '20180425--mivova--padscape--beat.mp3',
+        tempo: 122,
+        duration: 8,
+        part: 'drums',
+        startBeats: [0, 3],
+        endBeats: [0.5],
+      }
+    }
+  }
 };
 
 
 function getSample (url, audioContext, cb) {
-   var request = new XMLHttpRequest();
-   request.open('GET', url);
-   request.responseType = 'arraybuffer';
-   request.onload = function () {
-      console.log('sample loaded, decoding');
-      audioContext.decodeAudioData(request.response, cb);
-   };
-   console.log('loading sample');
-   request.send();
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'arraybuffer';
+  request.onload = function () {
+    console.log('sample loaded, decoding');
+    audioContext.decodeAudioData(request.response, cb);
+  };
+  console.log('loading sample');
+  request.send();
 }
 
-// this routine is probable part of what we want from sequencer/bpm-utilities
+// this routine is probably part of what we want from sequencer/bpm-utilities
 function getTimeOffsetForBeat(eventBeat, renderStart, renderEnd, transportBpm, wrapBeats) {  
-   let beatOffset = eventBeat - renderStart;
-   if ((renderEnd < renderStart) && (eventBeat < renderStart)) {
+  let beatOffset = eventBeat - renderStart;
+  if ((renderEnd < renderStart) && (eventBeat < renderStart)) {
       beatOffset += wrapBeats;
-   }
-   var offsetMs = bpmUtilities.beatsToMs(transportBpm, beatOffset);
-   return offsetMs;
+  }
+  var offsetMs = bpmUtilities.beatsToMs(transportBpm, beatOffset);
+  return offsetMs;
 }
 
 
 class ThrowdownAudioStem {
-   constructor(options) {
-      this.audioFile = options.audio;
-      this.tempo = options.tempo;
-      this.sampleLengthBeats = options.duration;
-      this.audioContext = options.audioContext;
-      this.secPerBeat = (60 / this.tempo);
+  constructor(options) {
+    this.audioFile = options.audio;
+    this.tempo = options.tempo;
+    this.sampleLengthBeats = options.duration;
+    this.audioContext = options.audioContext;
+    this.secPerBeat = (60 / this.tempo);
 
-      this.startBeats = options.startBeats || [0];
-      this.endBeats = options.endBeats || [0];
+    this.startBeats = options.startBeats || [0];
+    this.endBeats = options.endBeats || [0];
 
-      this.triggered = true;
-      this.playing = false;
+    this.triggered = true;
+    this.playing = false;
 
-      this.buffer = undefined;
+    this.buffer = undefined;
 
-      this.loaded = new Promise((resolve, reject) => {      
-         getSample(this.audioFile, this.audioContext, (buffer) => {
-            console.log('sample decoded, ready to play');
-            this.buffer = buffer;
-            resolve();
-         });
-      });
-   }
+    this.loaded = new Promise((resolve, reject) => {      
+      getSample(this.audioFile, this.audioContext, (buffer) => {
+      console.log('sample decoded, ready to play');
+      this.buffer = buffer;
+      resolve();
+    });
+  });
+}
 
-   playAt(startTimestamp, startBeat, transportBpm, audioDestinationNode) {
-      let rate = transportBpm / this.tempo;
+playAt(startTimestamp, startBeat, transportBpm, audioDestinationNode) {
+  let rate = transportBpm / this.tempo;
     // console.log(rate, this.audioContext.currentTime, time);
 
     this.player = this.audioContext.createBufferSource();
@@ -93,115 +93,106 @@ class ThrowdownAudioStem {
 
     // this.player.start();
     this.player.start(startTimestamp, startBeat * this.secPerBeat);
- }
+  }
 
- stopAt(stopTimestamp) {
+  stopAt(stopTimestamp) {
     if (this.player) {
-       this.player.stop(stopTimestamp);
-       console.log('stopping audio!!');
-    }
-    this.playing = false;
+      this.player.stop(stopTimestamp);
+      console.log('stopping audio!!');
+   }
+   this.playing = false;
  }
 
  updateAndRender(renderRange, triggerState, audioDestinationNode) {
-    if (!this.loaded) 
-       return;
+  if (!this.loaded) 
+    return;
 
-    this.triggered = triggerState;
+  this.triggered = triggerState;
 
-    let triggerInfo = patternSequencer.renderPatternTrigger(
-       renderRange, 
-       this.triggered,
-       this.playing, 
-       this.sampleLengthBeats,
-       this.startBeats,
-       this.endBeats,
-       );
-    this.playing = triggerInfo.isPlaying;
+  let triggerInfo = patternSequencer.renderPatternTrigger(
+    renderRange, 
+    this.triggered,
+    this.playing, 
+    this.sampleLengthBeats,
+    this.startBeats,
+    this.endBeats,
+  );
+  this.playing = triggerInfo.isPlaying;
 
     // here we "render" a single note-like event for the onset or offset of the audio stem
     // the API supports this but makes it feel strange .. 
     // e.g. fake ignored duration value
     let triggerEvents = [];
     if (triggerInfo.triggerOnset >= 0) {
-       
+      triggerEvents = patternSequencer.renderPatternEvents(renderRange, this.sampleLengthBeats, [{ start: triggerInfo.triggerOnset, duration: -1 }]);
+      let time = (triggerEvents[0].start + renderRange.audioContextTimeOffsetMsec) / 1000;
+      let beat = triggerInfo.triggerOnset;
+      console.log(`audio  onset t=${time.toFixed(2)} b=${beat.toFixed(2)} c=${this.audioContext.currentTime} ~c=${renderRange.audioContextTimeOffsetMsec}`)
 
-
-       /////
-       // pass logging option into renderpatternevents from here and for test beep and work out why this is different
-       triggerEvents = patternSequencer.renderPatternEvents
-          (renderRange, this.sampleLengthBeats, [{ start: triggerInfo.triggerOnset, duration: -1 }], true);
-        //(renderRange.start.time, triggerInfo, 4, filteredBeeps, true);
-       
-
-       let time = (triggerEvents[0].start + renderRange.audioContextTimeOffsetMsec) / 1000;
-       let beat = triggerInfo.triggerOnset;
-       console.log(`audio  onset t=${time.toFixed(2)} b=${beat.toFixed(2)} c=${this.audioContext.currentTime} ~c=${renderRange.audioContextTimeOffsetMsec}`)
-
-       this.playAt(time, triggerInfo.triggerOnset, renderRange.tempoBpm, audioDestinationNode);
+      this.playAt(time, triggerInfo.triggerOnset, renderRange.tempoBpm, audioDestinationNode);
     }
     else if (triggerInfo.triggerOffset >= 0) {
+      triggerEvents = patternSequencer.renderPatternEvents(renderRange, this.sampleLengthBeats, [{ start: triggerInfo.triggerOffset, duration: -1 }]);
+      let time = (triggerEvents[0].start + renderRange.audioContextTimeOffsetMsec) / 1000;
+      let beat = triggerInfo.triggerOffset;
+      console.log(`audio offset t=${time.toFixed(2)} b=${beat.toFixed(2)} c=${this.audioContext.currentTime} ~c=${renderRange.audioContextTimeOffsetMsec}`)
 
-       triggerEvents = patternSequencer.renderPatternEvents(renderRange, this.sampleLengthBeats, [{ start: triggerInfo.triggerOffset, duration: -1 }]);
-       let time = (triggerEvents[0].start + renderRange.audioContextTimeOffsetMsec) / 1000;
-       let beat = triggerInfo.triggerOffset;
-       console.log(`audio offset t=${time.toFixed(2)} b=${beat.toFixed(2)} c=${this.audioContext.currentTime} ~c=${renderRange.audioContextTimeOffsetMsec}`)
-
-       this.stopAt(time);    
+      this.stopAt(time);    
     }
- }
+  }
 }
 
 class Throwdown {
-   constructor(options) {
-      this.name = options.name || 'test';
-      this.tempo = options.tempo || 120;
-      this.key = options.key || 'C';
-      this.audioContext = options.audioContext;
+  constructor(options) {
+    this.name = options.name || 'test';
+    this.tempo = options.tempo || 120;
+    this.key = options.key || 'C';
+    this.audioContext = options.audioContext;
 
-      this.audioStems = _.filter(options.patterns, (patternData) => {
-         return patternData.audio.length > 0;
-      }).map((patternData) => {
-         return new ThrowdownAudioStem({
-            tempo: this.tempo, 
-            audioContext: this.audioContext,
-            ...patternData
-         });
+    this.audioStems = _.filter(options.patterns, (patternData) => {
+      return patternData.audio.length > 0;
+    }).map((patternData) => {
+    
+      return new ThrowdownAudioStem({
+        tempo: this.tempo, 
+        audioContext: this.audioContext,
+        ...patternData
       });
-   }
+    });
+  }
 
-   updateAndRender(renderRange, triggerState, midiOutPort, audioDestinationNode) {
-      _.map(this.audioStems, (audioStem) => {
-         audioStem.updateAndRender(renderRange, triggerState, audioDestinationNode);
-      });
-   }
+  updateAndRender(renderRange, triggerState, midiOutPort, audioDestinationNode) {
+    _.map(this.audioStems, (audioStem) => {
+      audioStem.updateAndRender(renderRange, triggerState, audioDestinationNode);
+    });
+  }
 
-   stop() {
-      _.map(this.audioStems, (audioStem) => {
-         audioStem.stopAt(0);
-      });
-   }
+  stop() {
+    _.map(this.audioStems, (audioStem) => {
+      audioStem.stopAt(0);
+    });
+  }
 }
 
 
 let mivova;
 
 const renderThrowdown = (renderRange, triggerState, midiOutPort) => {
-   const audioDestinationNode = renderRange.audioContext.destination;
-   if (!mivova)
-      mivova = new Throwdown({
-         audioContext: renderRange.audioContext,
-         ...throwdowns.mivova
-      });
-   mivova.updateAndRender(renderRange, triggerState, midiOutPort, audioDestinationNode);
+  const audioDestinationNode = renderRange.audioContext.destination;
+  if (!mivova)
+    mivova = new Throwdown({
+  audioContext: renderRange.audioContext,
+    ...throwdowns.mivova
+  });
+  mivova.updateAndRender(renderRange, triggerState, midiOutPort, audioDestinationNode);
 }
 
 const stopThrowdown = () => {
-   if (mivova)
-      mivova.stop();
+  if (mivova)
+    mivova.stop();
 }
 
 export default {
-   render: renderThrowdown,
-   stop: stopThrowdown
+  render: renderThrowdown,
+  stop: stopThrowdown
 }
