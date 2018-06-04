bae1ad7769770d5f0e65a4a858af6ec92507b4cf
haszari <haszari@cartoonbeats.com>
2018-06-04 15:35:01 +1200

    clean up commented stuff in top-level component

--- src/www/apps/throwdown/app.jsx
+++ src/www/apps/throwdown/app.jsx
@@ -9,13 +9,10 @@ import { Provider } from 'react-redux'
 
 import shortid from 'shortid';
 
-// import kytaimePatternSequencer from './kytaime-pattern-sequencer';
-
 import store from './stores/store';
 // import * as actions from './stores/actions';
 
 import Transport from './components/transport.jsx';
-// import PatternGrid from './components/container/pattern-grid.js';
 
 function App() {
    return (
@@ -33,53 +30,3 @@ document.body.appendChild(appDiv);
 
 render(<App/>, appDiv);
 
-// // block file drop redirect
-// document.addEventListener('dragover', event => event.preventDefault())
-// document.addEventListener('drop', event => event.preventDefault())
-
-// // initial state 
-// let numRows = 6;
-// store.dispatch(actions.setGridNumRows(numRows));
-// for (let i=0; i<numRows; i++)
-//    store.dispatch(actions.setGridRowMidiChannel({ rowIndex: i, midiChannel: i+1 }));
-
-
-
-// // budgo ui
-// window.shortid = shortid;
-
-// window.ui = { 
-//    setGridRows: (numRows) => {
-//       store.dispatch(actions.setGridNumRows(numRows));
-//    },
-//    setRowChannel: (rowIndex, midiChannel) => {
-//       store.dispatch(actions.setGridRowMidiChannel({ rowIndex, midiChannel }))
-//    }
-// }
-// window.exportProject = () => {
-//    return JSON.stringify(store.getState());
-// }
-// window.importProject = (json) => {
-//    let stateTree = JSON.parse(json);
-//    store.dispatch(actions.importRehydrate(stateTree)); 
-// } 
-
-
-// let importPattern = (channel, patternObject) => {
-//    store.dispatch(actions.addPattern({ 
-//       channel: channel,
-//       duration: patternObject.duration,
-//       notes: patternObject.notes,
-//       startBeats: patternObject.startBeats,
-//       endBeats: patternObject.endBeats
-//    }));
-// }
-
-// import {hats, kick, beat, bassline, lead, filter, send} from './lib/example-patterns';
-
-// importPattern(1, kick);
-// importPattern(1, hats);
-// importPattern(1, beat);
-// importPattern(2, bassline);
-// importPattern(3, lead);
-// importPattern(3, filter);
