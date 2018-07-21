4717dccb8f2177ee61953035148a7b23c9b99633
haszari <haszari@cartoonbeats.com>
2018-07-21 16:20:29 +0100

    pull in all of throwdown component/state except for snip stem list ui component

--- src/www/apps/squelcherisation/stores/reducers.js
+++ src/www/apps/squelcherisation/stores/reducers.js
@@ -2,9 +2,11 @@
 import { combineReducers } from 'redux'
 
 import transport from '../components/transport/index';
+import throwdown from '../components/throwdown/index';
 
 const app = combineReducers({
   transport: transport.reducer,
+  throwdown: throwdown.reducer,
 });
 
 export default app;
