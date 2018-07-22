d12b81800de07325da75c6e03a624875e23659a5
haszari <haszari@cartoonbeats.com>
2018-07-22 12:12:17 +0100

    squelch bassline notes

--- src/www/apps/squelcherisation/load-squelch-snip-data.js
+++ src/www/apps/squelcherisation/load-squelch-snip-data.js
@@ -18,9 +18,66 @@ store.dispatch(throwdownActions.throwdown_addSnipStem({
   } 
 }));
 
+store.dispatch(throwdownActions.throwdown_addSnipStem({ 
+  snip: 'squelcherisation', 
+  slug: 'bassline', 
+  data: {
+    notes: [
+      { 
+        start: 0, 
+        duration: 1.1,
+        note: 27 + 24,
+      },
+      { 
+        start: 1.5, 
+        duration: 0.5,
+        note: 22 + 24,
+      },
+      { 
+        start: 3, 
+        duration: 0.5,
+        note: 22 + 24,
+      },
+      { 
+        start: 4, 
+        duration: 3,
+        note: 15 + 24,
+      },
+      { 
+        start: 9, 
+        duration: 1,
+        note: 25 + 24,
+      },
+      { 
+        start: 11, 
+        duration: 1,
+        note: 25 + 24,
+      },
+      { 
+        start: 12, 
+        duration: 3,
+        note: 16 + 24,
+      },
+      { // is this one for real?
+        start: 15, 
+        duration: 1,
+        note: 18 + 24,
+      },
+    ],
+    duration: 16, // I think
+    // part: 'alpine',
+    // startBeats: [0, 3],
+    // endBeats: [0.5],
+  } 
+}));
+
 
 // we need a "set"; we don't know what toggle does if we don't know current state
 store.dispatch(throwdownActions.throwdown_toggleSnipStemTrigger({ 
   snip: 'squelcherisation', 
   slug:'beat',
+}));
+store.dispatch(throwdownActions.throwdown_toggleSnipStemTrigger({ 
+  snip: 'squelcherisation', 
+  slug: 'bassline',
 }));
