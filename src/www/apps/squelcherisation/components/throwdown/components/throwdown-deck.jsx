f277338cfb08405e7369214d0db25b3ea3eecc7d
haszari <haszari@cartoonbeats.com>
2018-08-11 10:55:34 +1200

    style decks (like a rounded rect)

--- src/www/apps/squelcherisation/components/throwdown/components/throwdown-deck.jsx
+++ src/www/apps/squelcherisation/components/throwdown/components/throwdown-deck.jsx
@@ -2,10 +2,19 @@
 import React from 'react';
 
 
+
+
 const ThrowdownDeck = (props) => {
   const { id } = props;
+  const style = {
+    backgroundColor: '#f8f8f8',
+    borderRadius: '0.2em',
+    padding: '1em 0',
+    marginBottom: '1.5em',
+  };
+
   return (
-    <div>Throwdown deck { id }</div>
+    <div style={ style }>Throwdown deck { id }</div>
   );
 }
 
