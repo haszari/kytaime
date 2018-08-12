e16cb715806daa842e8232451f647bd7418da555
haszari <haszari@cartoonbeats.com>
2018-08-12 19:43:03 +1200

    test data for the second deck

--- src/www/apps/squelcherisation/load-squelch-snip-data.js
+++ src/www/apps/squelcherisation/load-squelch-snip-data.js
@@ -74,7 +74,7 @@ const beatPatterns = {
 
 // we have two lines, because we're like a DJ, and they are like decks
 store.dispatch(throwdownActions.throwdown_addDeck());
-// store.dispatch(throwdownActions.throwdown_addDeck());
+store.dispatch(throwdownActions.throwdown_addDeck());
 
 // let's add some playable sections to the first deck
 store.dispatch(throwdownActions.throwdown_addSection({ 
@@ -157,6 +157,141 @@ store.dispatch(throwdownActions.throwdown_addSection({ deckId: 0, data: {} }));
 
 store.dispatch(throwdownActions.throwdown_setTriggeredSection({ deckId: 0, sectionId: 0 }));
 
+// let's add some cool stuff to the second deck
+store.dispatch(throwdownActions.throwdown_addSection({ 
+  deckId: 1,
+  data: {
+    parts: [{
+      label: 'chord', 
+      data: {  
+        audio: {
+          file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170703-stemcells-padscape-a3-stemprep--maenyb-chords.mp3'),
+          tempo: 133,
+        },
+        pattern: {
+          duration: 4,
+          slices: [
+            {
+              start: 1, 
+              duration: 1,
+              beat: 1,
+            },
+            {
+              start: 3, 
+              duration: 1,
+              beat: 1,
+            },
+          ]
+        },
+      }
+    }, {
+      label: 'arp', 
+      data: {  
+        audio: {
+          file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems/20180425--mivova--padscape--lead.mp3'),
+          tempo: 122,
+        },
+        pattern: {
+          duration: 16,
+          slices: [
+            {
+              start: 0, 
+              duration: 1,
+              beat: 16,
+            },
+            {
+              start: 3, 
+              duration: 1,
+              beat: 0,
+            },
+            {
+              start: 6, 
+              duration: 1,
+              beat: 0,
+            },
+            {
+              start: 9, 
+              duration: 1,
+              beat: 32,
+            },
+            {
+              start: 12, 
+              duration: 1,
+              beat: 48,
+            },
+            {
+              start: 15, 
+              duration: 1,
+              beat: 48,
+            },
+          ],
+        },
+      }
+    }]
+  }
+}));
+store.dispatch(throwdownActions.throwdown_addSection({ 
+  deckId: 1,
+  data: {
+    parts: [{
+      label: 'chord', 
+      data: {  
+        audio: {
+          file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170703-stemcells-padscape-a3-stemprep--maenyb-chords.mp3'),
+          tempo: 133,
+        },
+        pattern: {
+          duration: 8,
+          slices: [
+            {
+              start: 7, 
+              duration: 0.25,
+              beat: 5,
+            },
+            {
+              start: 7.25, 
+              duration: 0.25,
+              beat: 5,
+            },
+            {
+              start: 7.5, 
+              duration: 0.25,
+              beat: 1,
+            },
+            {
+              start: 7.75, 
+              duration: 0.25,
+              beat: 1,
+            },
+          ]
+        },
+      }
+    }, {
+      label: 'arp2', 
+      data: {  
+        audio: {
+          file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems/20180425--mivova--padscape--lead.mp3'),
+          tempo: 122,
+        },
+        pattern: {
+          duration: 8,
+          slices: [
+            {
+              start: 0, 
+              duration: 3,
+              beat: 16,
+            },
+            {
+              start: 3, 
+              duration: 5,
+              beat: 48,
+            },
+          ],
+        },
+      }
+    }]
+  }
+}));
 
 
 // more test actions
