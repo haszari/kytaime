eaa0f7f4d653d94ae10c3b1b77be63ebd7e997cc
haszari <haszari@cartoonbeats.com>
2018-08-12 19:13:45 +1200

    selectors for determining a good place to transition from one section to another, based on the patterns involved

--- src/www/apps/squelcherisation/components/throwdown/selectors.js
+++ src/www/apps/squelcherisation/components/throwdown/selectors.js
@@ -15,7 +15,24 @@ export const getSection = (state, { deckId, sectionId }) => {
     return _.find(deck.sections, { id: sectionId });
 }
 
+// this could come from state and be an option with UI etc
+// it's kinda like "breathing room"
+const minSectionPhraseDuration = 4; 
+
 export const getSectionPhraseDuration = (state, { deckId, sectionId }) => {
-  // we'll hard code this to begin with!
-  return 32;
+  const section = getSection(state, { deckId, sectionId });
+  if (section && section.data && section.data.parts)
+    return _.reduce(section.data.parts, (accum, part) => {
+      const patternDuration = (part.data && part.data.pattern) ? part.data.pattern.duration : 0;
+      return Math.max(patternDuration, accum)
+    }, minSectionPhraseDuration);
+  return minSectionPhraseDuration;
+}
+
+// same thing with array?
+export const getPhraseDuration = (state, { deckId, sectionIds }) => {
+  const bim = _.reduce(sectionIds, (accum, sectionId) => {
+    return Math.max(getSectionPhraseDuration(state, { deckId, sectionId }), accum);
+  }, minSectionPhraseDuration);
+  return bim;
 }
