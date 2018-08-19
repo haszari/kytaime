804f17248dfea6c467e00b06b57ec9310bc8e96f
haszari <haszari@cartoonbeats.com>
2018-08-19 16:45:53 +1200

    set up 3 snips from padded landscape for testing/experimenting/bug fixing

---
+++ src/www/load-paddedlandscape-snip-data.js
@@ -0,0 +1,195 @@
+import store from './stores/store';
+
+import * as deckActions from './components/decks/actions';
+
+////---------------------------------------
+//// Noyu
+
+store.dispatch(deckActions.throwdown_addDeck());
+
+const noyu = {
+  tempo: 120,
+  deckId: 0,
+};
+
+
+store.dispatch(deckActions.throwdown_addSection({ 
+  deckId: noyu.deckId,
+  parts: [{
+    slug: 'beat', 
+    part: 'drums',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--noyu-alpine-beat.mp3'),
+        tempo: noyu.tempo,
+      },
+      pattern: {
+        duration: 32,
+      },
+    }
+  }, {
+    slug: 'bass', 
+    part: 'bass',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--noyu-ridge-bass.mp3'),
+        tempo: noyu.tempo,
+      },
+      pattern: {
+        duration: 8,
+      },
+    }
+  }, {
+    slug: 'chords', 
+    part: 'chords',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--noyu-uplands-chords.mp3'),
+        tempo: noyu.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }, {
+    slug: 'piano', 
+    part: 'lead',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--noyu-hills-piano.mp3'),
+        tempo: noyu.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }]
+}));
+
+
+////---------------------------------------
+//// Kufca
+
+const kufca = {
+  tempo: 126,
+  deckId: 1,
+};
+
+store.dispatch(deckActions.throwdown_addDeck());
+
+store.dispatch(deckActions.throwdown_addSection({ 
+  deckId: kufca.deckId,
+  parts: [{
+    slug: 'beat', 
+    part: 'drums',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170806-kufca-padscape-beat-alpine.mp3'),
+        tempo: kufca.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }, {
+    slug: 'sub', 
+    part: 'bass',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170806-kufca-padscape-sub-ridge.mp3'),
+        tempo: kufca.tempo,
+      },
+      pattern: {
+        duration: 16,
+      },
+    }
+  }, {
+    slug: 'lead', 
+    part: 'synth',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170806-kufca-padscape-lead-uplands.mp3'),
+        tempo: kufca.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }, {
+    slug: 'texture', 
+    part: 'texture',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170806-kufca-padscape-texture-hills.mp3'),
+        tempo: kufca.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }]
+}));
+
+////---------------------------------------
+//// Peaches (Stickity)
+
+const peaches = {
+  tempo: 132,
+  deckId: 2,
+};
+
+store.dispatch(deckActions.throwdown_addDeck());
+
+store.dispatch(deckActions.throwdown_addSection({ 
+  deckId: peaches.deckId,
+  parts: [{
+    slug: 'beat', 
+    part: 'drums',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--peaches-alpine-beat.mp3'),
+        tempo: peaches.tempo,
+      },
+      pattern: {
+        duration: 16,
+      },
+    }
+  }, {
+    slug: 'sub', 
+    part: 'bass',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--peaches-ridge-sub.mp3'),
+        tempo: peaches.tempo,
+      },
+      pattern: {
+        duration: 16,
+      },
+    }
+  }, {
+    slug: 'lead', 
+    part: 'synth',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--peaches-uplands-lead.mp3'),
+        tempo: peaches.tempo,
+      },
+      pattern: {
+        duration: 64,
+      },
+    }
+  }, {
+    slug: 'texture', 
+    part: 'texture',
+    data: {  
+      audio: {
+        file: encodeURI('/media/Haszari/Haszari Renders - Snips Stems Padded Landscape/20170816--padscape-stemprep--peaches-hills-texture.mp3'),
+        tempo: peaches.tempo,
+      },
+      pattern: {
+        duration: 32,
+      },
+    }
+  }]
+}));
+
