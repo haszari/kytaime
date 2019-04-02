bb6444ac315dc8be39bce55cadfc8fde1e196e32
haszari <haszari@cartoonbeats.com>
2019-04-02 22:06:39 +1300

    move file load code to a module

---
+++ src/apps/throwdown/components/file-import/file-import.js
@@ -0,0 +1,79 @@
+import _ from 'lodash'; 
+
+import Hjson from 'hjson';
+
+import store from '../../store/store';
+
+import throwdownActions from '../throwdown/actions';
+import throwdownSelectors from '../throwdown/selectors';
+
+import sequencer from '@kytaime/sequencer/sequencer';
+import audioUtilities from '@kytaime/audio-utilities';
+
+function ensureAudioBuffered( audioContext, buffers, filename ) {
+  if ( _.find( buffers, { file: filename } ) ) {
+    return;
+  }
+  audioUtilities.loadSample( filename, audioContext, ( buffer ) => {
+    console.log( `sample decoded, ready to play ${ filename }` );
+    store.dispatch( throwdownActions.addAudioBuffer( {
+      file: filename,
+      buffer: buffer,
+    } ) );
+  } );
+}
+
+function importPatterns( songSlug, patterns ) {
+  const buffers = throwdownSelectors.getBuffers( store.getState() );
+
+  _.map( patterns, ( pattern, key ) => {
+    store.dispatch( throwdownActions.addPattern( {
+      songSlug, 
+      slug: key, 
+      ...pattern
+    } ) );
+    if ( pattern.file ) {
+      ensureAudioBuffered( sequencer.audioContext, buffers, pattern.file );
+    }
+  } );  
+}
+
+function addThrowdownDeck( songSlug, throwdownData ) {
+  importPatterns( songSlug, throwdownData.patterns );
+
+  store.dispatch( throwdownActions.addDeck( {
+    deckSlug: songSlug,
+  } ) ); 
+  
+  _.map( throwdownData.sections, ( section, key ) => {
+    store.dispatch( throwdownActions.addSection( {
+      deckSlug: songSlug,
+      slug: key, 
+      ...section
+    } ) );
+  } );
+
+  // trigger a random section
+  // const sectionSlugs = _.keys( throwdownData.sections );
+  // store.dispatch( throwdownActions.setDeckTriggeredSection( {
+  //   deckSlug: songSlug,
+  //   sectionSlug: _.sample( sectionSlugs )
+  // } ) );
+
+  // hard code build for testing
+  // store.dispatch( throwdownActions.setDeckTriggeredSection( {
+  //   deckSlug: 'test',
+  //   sectionSlug: 'build',
+  // } ) );
+}
+
+function importThrowdownFile( deckSlug, file ) {
+  window.fetch( file )
+    .then( response => response.text() )
+    .then( text => {
+      const songData = Hjson.parse( text );
+      addThrowdownDeck( deckSlug, songData );
+    } );
+}
+
+export default importThrowdownFile;
--- src/apps/throwdown/index.js
+++ src/apps/throwdown/index.js
@@ -1,11 +1,7 @@
-import _ from 'lodash'; 
-
 import React from 'react';
 import { render } from 'react-dom';
 
 import { Provider } from 'react-redux';
-
-import Hjson from 'hjson';
 
 import store from './store/store';
 import observeStore from '@lib/observe-redux-store';
@@ -16,8 +12,7 @@ import Header from './components/transport-header/header.jsx';
 import HeaderPlaybackProgress from './components/playback-progress/header-progress.jsx';
 import Decks from './components/throwdown/decks.jsx';
 
-
-import throwdownActions from './components/throwdown/actions';
+import importThrowdownFile from './components/file-import/file-import';
 
 import './style/style.scss';
 
@@ -26,51 +21,8 @@ import './style/style.scss';
 
 const throwdownApp = new ThrowdownApp();
 
-
 /// -----------------------------------------------------------------------------------------------
 // load hard-coded test data
-
-function addThrowdownDeck( songSlug, throwdownData ) {
-  throwdownApp.importPatterns( songSlug, throwdownData.patterns );
-
-  store.dispatch( throwdownActions.addDeck( {
-    deckSlug: songSlug,
-  } ) ); 
-  
-  _.map( throwdownData.sections, ( section, key ) => {
-    store.dispatch( throwdownActions.addSection( {
-      deckSlug: songSlug,
-      slug: key, 
-      ...section
-    } ) );
-  } );
-
-  // trigger a random section
-  // const sectionSlugs = _.keys( throwdownData.sections );
-  // store.dispatch( throwdownActions.setDeckTriggeredSection( {
-  //   deckSlug: songSlug,
-  //   sectionSlug: _.sample( sectionSlugs )
-  // } ) );
-
-  // hard code build for testing
-  // store.dispatch( throwdownActions.setDeckTriggeredSection( {
-  //   deckSlug: 'test',
-  //   sectionSlug: 'build',
-  // } ) );
-}
-
-// const testSongFile = '/data/20190217--manas.hjson';
-// const testSongFile = 'data/20190306--sweets-from-a-stranger.hjson';
-// const testSongFile = 'data/20190306--its-not-real.hjson';
-
-function importThrowdownFile( deckSlug, file ) {
-  window.fetch( file )
-    .then( response => response.text() )
-    .then( text => {
-      const songData = Hjson.parse( text );
-      addThrowdownDeck( deckSlug, songData );
-    } );
-}
 
 importThrowdownFile( 'manas', '/data/20190217--manas.hjson' );
 // importThrowdownFile( 'noyu', '/data/20190325--noyu.hjson' );
@@ -79,7 +31,7 @@ importThrowdownFile( 'manas', '/data/20190217--manas.hjson' );
 // importThrowdownFile( 'maenyb', 'data/20190325--maenyb.hjson' );
 // importThrowdownFile( 'shedout', 'data/20190325--shedout.hjson' );
 // importThrowdownFile( 'mivova', 'data/20190325--mivova.hjson' );
-// importThrowdownFile( 'kufca', 'data/20190325--kufca.hjson' );
+importThrowdownFile( 'kufca', 'data/20190325--kufca.hjson' );
 
 /// -----------------------------------------------------------------------------------------------
 // bind sequencer/transport to store
