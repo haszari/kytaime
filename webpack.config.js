29a929ec0e6e7f1c035830091fc7ee7fccf55c8a
haszari <haszari@cartoonbeats.com>
2022-03-13 11:08:18 +1300

    fixes for webpack 5: - use eslint webpack plugin for webpack v5 - webpack dev server middleware v5 option syntax onBeforeSetupMiddleware - add in browserify, needed for `os` node API polyfill for hjson lib - webpack dev server contentBase => static

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,62 @@
-const express = require('express');
-const path = require('path');
+const ESLintPlugin = require("eslint-webpack-plugin");
+const express = require("express");
+const path = require("path");
 
-const aliases = require('./webpack-aliases');
+const aliases = require("./webpack-aliases");
 
 const Config = {
-  entry: {
-    main: './src/apps/throwdown/index.js',
-    'import-midi': './src/apps/import-midi/index.js'
-  },
-  mode: process.env['NODE_ENV'] || 'production',
-  devtool: process.env['NODE_ENV'] === 'development' ? 'source-map' : false,
-  resolve: {
-    modules: [
-      'node_modules'
-    ],
-    alias: aliases,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-            "style-loader", // creates style nodes from JS strings
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
+   plugins: [new ESLintPlugin()],
+   entry: {
+      main: "./src/apps/throwdown/index.js",
+      "import-midi": "./src/apps/import-midi/index.js",
+   },
+   mode: process.env["NODE_ENV"] || "production",
+   devtool: process.env["NODE_ENV"] === "development" ? "source-map" : false,
+   resolve: {
+      modules: ["node_modules"],
+      alias: aliases,
+      fallback: { "os": require.resolve("os-browserify/browser") },
+   },
+   module: {
+      rules: [
+         {
+            test: /\.scss$/,
+            use: [
+               "style-loader", // creates style nodes from JS strings
+               "css-loader", // translates CSS into CommonJS
+               "sass-loader", // compiles Sass to CSS, using Node Sass by default
+            ],
+         },
+         {
+            test: /\.worker\.js$/,
+            use: { loader: "worker-loader" },
+         },
+         {
+            test: /\.js$|\.jsx$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+         },
+      ],
+   },
+   devServer: {
+      onBeforeSetupMiddleware: function (devServer) {
+         // serve iTunes library for samples/media
+         devServer.app.use(
+            "/media",
+            express.static(
+               path.join(process.env.HOME, "/Music/iTunes/iTunes Media/Music")
+            )
+         );
+         // serve hjson song data from src
+         devServer.app.use(
+            "/data",
+            express.static(path.join(__dirname, "/src/data"))
+         );
       },
-      {
-        test: /\.worker\.js$/,
-        use: { loader: 'worker-loader' }
-      },
-      {
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader?fix=true&emitWarning=true']
-      }
-    ]
-  },
-  devServer: {
-    before: function(app, server) {
-      // serve iTunes library for samples/media
-      app.use(
-        '/media', 
-        express.static(
-          path.join( process.env.HOME, '/Music/iTunes/iTunes Media/Music' )
-        )
-      );
-      // serve hjson song data from src
-      app.use(
-        '/data', 
-        express.static(
-          path.join( __dirname, '/src/data' )
-        )
-      );
-    },
-    contentBase: path.join( __dirname, '/src/html-routes' ),
-    compress: true,
-    port: 3876
-  }
+      static: path.join(__dirname, "/src/html-routes"),
+      compress: true,
+      port: 3876,
+   },
 };
 
 // if (!(process.env['NODE_ENV'] === 'development')) {
